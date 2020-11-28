8ea8b0aac3bb2eed33a68be5d278f7fe402a782f
Matt Austin <mattaustin@macbook-pro.lan>
2020-11-27 19:23:32 -0500

    added a button

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from '@material-ui/core/Button';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
         Welcome to mattpaustin.com
         <div>
           *** site under construction ***
+        </div>
+        <div>
+          <Button variant='outlined' style={{margin: '30px', color: 'white'}} >
+            Don't click this button
+          </Button>
         </div>
       </header>
     </div>
