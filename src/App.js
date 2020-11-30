545f4e06e1313cecd74c8dc3276b3f63884441e9
Matt Austin <mpaustin13@gmail.com>
2020-11-30 12:37:49 -0500

    various design changes

--- src/App.js
+++ src/App.js
@@ -6,17 +6,6 @@ import Framework from './templates/Framework';
 function App() {
   return (
     <div className="App">
-      {/* <header className="App-header">
-        Welcome to mattpaustin.com
-        <div>
-          *** site under construction ***
-        </div>
-        <div>
-          <Button variant='outlined' style={{margin: '30px', color: 'white'}} >
-            Don't click this button
-          </Button>
-        </div>
-      </header> */}
       <Framework />
     </div>
   );
--- src/templates/Framework.js
+++ src/templates/Framework.js
@@ -1,11 +1,5 @@
 import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
-import Button from '@material-ui/core/Button';
-import CameraIcon from '@material-ui/icons/PhotoCamera';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Grid from '@material-ui/core/Grid';
 import Toolbar from '@material-ui/core/Toolbar';
@@ -20,6 +14,7 @@ import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import IconButton from '@material-ui/core/IconButton';
 import GitHubIcon from '@material-ui/icons/GitHub';
 import InstagramIcon from '@material-ui/icons/Instagram';
+import Box from '@material-ui/core/Box';
 
 function Copyright() {
   return (
@@ -47,7 +42,7 @@ const useStyles = makeStyles((theme) => ({
     fontFamily: 'EB Garamond,serif',
   },
   heroButtons: {
-    marginTop: theme.spacing(4),
+    // marginTop: theme.spacing(4),
     // color: '#49b5eb',
   },
   heroButtonsPrimary: {
@@ -87,36 +82,12 @@ const useStyles = makeStyles((theme) => ({
     cursor: 'pointer',
   },
   motionW: {
-    height: theme.spacing(8),
-    width: theme.spacing(8),
-    margin: 'auto',
-    marginBottom: '30px',
+    height: theme.spacing(4),
+    width: theme.spacing(4),
+    // margin: 'auto',
+    marginRight: '15px',
   },
 }));
-
-const cards = [
-    {
-        heading: 'House Hack Knack',
-        copy: 'My (retired (for now)) blog about REI',
-        image: 'https://source.unsplash.com/photos/A3DPhhAL6Zg',
-        imageTitle: '',
-        url: 'https://www.househackknack.com',
-    },
-    {
-        heading: 'Go Badgers',
-        copy: 'Another disappointing football season...',
-        image: 'https://source.unsplash.com/photos/A3DPhhAL6Zg',
-        imageTitle: '',
-        url: 'https://www.uwbadgers.com',
-    },
-    {
-        heading: 'Soothe your soul',
-        copy: 'What\'s your favorite Chris Stapleton song?',
-        image: 'https://source.unsplash.com/photos/A3DPhhAL6Zg',
-        imageTitle: '',
-        url: 'https://www.chrisstapleton.com',
-    },
-];
 
 const openUrl = (url) => {
     window.open(url, '_blank');
@@ -129,12 +100,7 @@ export default function Album() {
     <React.Fragment>
       <CssBaseline />
       <AppBar position="relative" className={classes.appBar} >
-        <Toolbar>
-          {/* <CameraIcon className={classes.icon} /> */}
-          {/* <Typography variant="h6" color="inherit" noWrap>
-            mattpaustin
-          </Typography> */}
-        </Toolbar>
+        <Toolbar/>
       </AppBar>
       <main>
         <div className={classes.heroContent}>
@@ -143,14 +109,14 @@ export default function Album() {
             <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom className={classes.title}>
               Matt Austin
             </Typography>
-            <div>
+            <Box display='flex' flexDirection='row' justifyContent='center'>
                 <Avatar src={MotionW} alt='motionW' variant='square' className={classes.motionW} />
                 <Typography variant="h5" align="center" color="textSecondary" paragraph className={classes.subtitle}>
                     <Link color='inherit' onClick={() => openUrl('https://www.engr.wisc.edu')}>
                         BSc, University of Wisconsin-Madison
                     </Link>
                 </Typography>
-            </div>
+            </Box>
             <div className={classes.heroButtons}>
               <Grid container spacing={2} justify="center">
                 <Grid item>
@@ -172,39 +138,8 @@ export default function Album() {
             </div>
           </Container>
         </div>
-        {/* <Container className={classes.cardGrid} maxWidth="md">
-          <Grid container spacing={4}>
-            {cards.map((card) => (
-              <Grid item key={card} xs={12} sm={6} md={4}>
-                <Card className={classes.card}>
-                  <CardMedia
-                    className={classes.cardMedia}
-                    image={card.image}
-                    title={card.imageTitle}
-                  />
-                  <CardContent className={classes.cardContent}>
-                    <Typography gutterBottom variant="h5" component="h2">
-                      {card.heading}
-                    </Typography>
-                    <Typography>
-                      {card.copy}
-                    </Typography>
-                  </CardContent>
-                  <CardActions>
-                    <Button size="small" color="primary" onClick={() => openUrl(card.url)}>
-                      View
-                    </Button>
-                  </CardActions>
-                </Card>
-              </Grid>
-            ))}
-          </Grid>
-        </Container> */}
       </main>
       <footer className={classes.footer}>
-        {/* <Typography variant="h6" align="center" gutterBottom>
-          Thanks for visiting!
-        </Typography> */}
         <Copyright />
       </footer>
     </React.Fragment>
