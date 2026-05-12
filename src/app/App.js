1498499e5aa4a84db01ba2fe6cc9d78057844a56
Matt <matt@notablefi.com>
2026-05-12 13:05:03 -0500

    Add resume page

--- src/app/App.js
+++ src/app/App.js
@@ -1,10 +1,31 @@
+import React from 'react';
 import './App.css';
 import HomePage from '../pages/HomePage';
+import ResumePage from '../pages/ResumePage';
 
 function App() {
+  const [path, setPath] = React.useState(window.location.pathname);
+
+  React.useEffect(() => {
+    const handlePopState = () => {
+      setPath(window.location.pathname);
+    };
+
+    window.addEventListener('popstate', handlePopState);
+
+    return () => {
+      window.removeEventListener('popstate', handlePopState);
+    };
+  }, []);
+
+  const navigate = (nextPath) => {
+    window.history.pushState({}, '', nextPath);
+    setPath(nextPath);
+  };
+
   return (
     <div className='App'>
-      <HomePage />
+      {path === '/resume' ? <ResumePage /> : <HomePage navigate={navigate} />}
     </div>
   );
 }
